52b9068fd874c70abd50082fad45f4366bd1de60
belo <pesadam15@gmail.com>
2023-02-27 01:04:53 +0100

    routes, views

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function(){
+    return view('products');
+});
+
+Route::get('/product', function(){
+    return view('product');
+});
+
+Route::get('/cart', function(){
+    return view('cart');
+});
+
+Route::get('/checkout', function(){
+    return view('checkout');
+});
+
+Route::get('/thankyou', function(){
+    return view('thankyou');
+});
